8acfc22e7ec3917cde8235b64485c024b862d68b
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-16 13:19:45 +0530

    Enough Space
    Question Link :- https://www.codechef.com/problems/ENSPACE?tab=submissions

---
+++ C10.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+       int a,b,c; cin>>a>>b>>c;
+       int pro = b + (c*2);
+       if(pro <= a){
+           cout<<"YES"<<endl;
+       }
+       else{
+           cout<<"NO"<<endl;
+       }
+        
+        
+    }
+    
+
+
+
+}
