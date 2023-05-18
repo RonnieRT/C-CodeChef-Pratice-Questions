bc29333e11707b73797f555e7b6f0c8186d0bf2a
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-19 01:26:20 +0530

    Determining The Score
    Question Link :- https://www.codechef.com/problems/DETSCORE

---
+++ C16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,n;
+	    cin>>x>>n;
+	    int count=((x*n)/10);
+	    cout<<count<<endl;
+	    
+	}
+	return 0;
+}
