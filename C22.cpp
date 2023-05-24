0ad6a0d54bb9b37e2d5ee362cd66649309379345
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-24 20:46:51 +0530

    Max Minus Min
    Question Link :- https://www.codechef.com/problems/MAXDIFFMIN?tab=submissions

---
+++ C22.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,c;
+        cin>>a>>b>>c;
+        cout<<c-a<<endl;
+    }
+}
